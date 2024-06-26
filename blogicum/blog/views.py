c72a8e7879737966b4aa6979b943d5144066c99c
Alex Shomin <alex.sh0min@yandex.com>
2024-06-27 01:54:05 +0700

    bad test

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -4,52 +4,6 @@ from .models import Post, Category
 from django.utils.timezone import now
 
 
-posts = [
-    {
-        'id': 0,
-        'location': 'Остров отчаянья',
-        'date': '30 сентября 1659 года',
-        'category': 'travel',
-        'text': '''Наш корабль, застигнутый в открытом море
-                страшным штормом, потерпел крушение.
-                Весь экипаж, кроме меня, утонул; я же,
-                несчастный Робинзон Крузо, был выброшен
-                полумёртвым на берег этого проклятого острова,
-                который назвал островом Отчаяния.''',
-    },
-    {
-        'id': 1,
-        'location': 'Остров отчаянья',
-        'date': '1 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Проснувшись поутру, я увидел, что наш корабль сняло
-                с мели приливом и пригнало гораздо ближе к берегу.
-                Это подало мне надежду, что, когда ветер стихнет,
-                мне удастся добраться до корабля и запастись едой и
-                другими необходимыми вещами. Я немного приободрился,
-                хотя печаль о погибших товарищах не покидала меня.
-                Мне всё думалось, что, останься мы на корабле, мы
-                непременно спаслись бы. Теперь из его обломков мы могли бы
-                построить баркас, на котором и выбрались бы из этого
-                гиблого места.''',
-    },
-    {
-        'id': 2,
-        'location': 'Остров отчаянья',
-        'date': '25 октября 1659 года',
-        'category': 'not-my-day',
-        'text': '''Всю ночь и весь день шёл дождь и дул сильный
-                порывистый ветер. 25 октября.  Корабль за ночь разбило
-                в щепки; на том месте, где он стоял, торчат какие-то
-                жалкие обломки,  да и те видны только во время отлива.
-                Весь этот день я хлопотал  около вещей: укрывал и
-                укутывал их, чтобы не испортились от дождя.''',
-    },
-]
-
-POSTS_DICT = {post['id']: post for post in posts}
-
-
 def index(request):
     posts = Post.objects.select_related(
         'author', 'category', 'location',
@@ -78,16 +32,13 @@ def post_detail(request, post_id):
 
 def category_posts(request, category_slug):
     category = get_object_or_404(
-        Category.objects.filter(
-            slug=category_slug,
-            is_published=True
-        )
+        Category,
+        slug=category_slug,
+        is_published=True
     )
-    post_list = Post.objects.filter(
+    post_list = category.post_set.filter(
         is_published=True,
-        pub_date__lt=now(),
-        category__slug=category_slug,
+        pub_date__lt=now()
     )
-
     return render(request, 'blog/category.html',
                   {'post_list': post_list, 'category': category})
